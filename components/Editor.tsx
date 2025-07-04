92fcda102a69d63ad7fb13000ef2f2113a6fb8f6
Dell <muhammadsarmad2003@email.com>
2025-07-04 17:37:26 +0500

    feat: commit as myself with proper Git identity

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,16 +1,69 @@
 " use client";
-import { useRoom } from "@liveblocks/react";
-import { useState } from "react";
+import { useRoom, useSelf } from "@liveblocks/react";
+import { useEffect, useState } from "react";
 import * as Y from "yjs";
 import { LiveblocksYjsProvider } from "@liveblocks/yjs";
 import { Button } from "./ui/button";
 import { MoonIcon, SunIcon } from "lucide-react";
+import { BlockNoteView } from "@blocknote/shadcn";
+import { BlockNoteEditor } from "@blocknote/core";
+import { useCreateBlockNote } from "@blocknote/react";
+import "@blocknote/core/fonts/inter.css";
+import "@blocknote/shadcn/style.css";
+import stringToColor from "@/lib/stringToColor";
+
+type EditorProps = {
+  doc: Y.Doc;
+  provider: any;
+  darkMode: boolean;
+};
+
+function BlockNote({ doc, provider, darkMode }: EditorProps) {
+  const userInfo = useSelf((me) => me.info);
+
+  const editor: BlockNoteEditor = useCreateBlockNote({
+    collaboration: {
+      provider,
+      fragment: doc.getXmlFragment("document-store"),
+
+      user: {
+        name: userInfo?.name ?? "Anonymous",
+        color: stringToColor(userInfo?.email || "1"),
+      },
+    },
+  });
+  return (
+    <div className="relative max-w-6xl mx-auto">
+      <BlockNoteView
+        className="min-h-screen"
+        editor={editor}
+        theme={darkMode ? "dark" : "light"}
+      />
+    </div>
+  );
+}
 
 function Editor() {
   const room = useRoom();
   const [doc, setDoc] = useState<Y.Doc>();
   const [provider, setProvider] = useState<LiveblocksYjsProvider>();
   const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const yDoc = new Y.Doc();
+    const yProvider = new LiveblocksYjsProvider(room, yDoc);
+    setDoc(yDoc);
+    setProvider(yProvider);
+
+    return () => {
+      yDoc?.destroy();
+      yProvider?.destroy();
+    };
+  }, [room]);
+
+  if (!doc || !provider) {
+    return null;
+  }
 
   const style = `hover: text-white ${
     darkMode
@@ -25,6 +78,7 @@ function Editor() {
           {darkMode ? <SunIcon /> : <MoonIcon />}
         </Button>
       </div>
+      <BlockNote doc={doc} provider={provider} darkMode={darkMode} />
     </div>
   );
 }
