5ecb034763e11b2dd06054bde3d6d9fb04a2fd34
Sarmad <muhammadsarmad2003@gmail.com>
2025-07-05 23:35:49 +0500

    fix: delete button now removes data from Firebase

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { adminDb } from "@/firebase-admin";
+import liveblocks from "@/lib/liveblocks";
 import { auth } from "@clerk/nextjs/server";
 
 export async function createNewDocument() {
@@ -26,4 +27,33 @@ export async function createNewDocument() {
     });
 
   return { docId: docRef.id };
+}
+
+export async function deleteDocument(roomId: string) {
+  auth.protect();
+
+  console.log("deleteDocument", roomId);
+
+  try {
+    await adminDb.collection("documents").doc(roomId).delete();
+
+    const query = await adminDb
+      .collection("rooms")
+      .where("roomId", "==", roomId)
+      .get();
+
+    const batch = adminDb.batch();
+    query.forEach((doc) => {
+      batch.delete(doc.ref);
+    });
+
+    await batch.commit();
+
+    await liveblocks.deleteRoom(roomId);
+
+    return { success: true };
+  } catch (error) {
+    console.error(error);
+    return { success: false };
+  }
 }
